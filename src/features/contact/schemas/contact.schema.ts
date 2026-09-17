a2a95df0eb9dd734f9c4779571f05770bd0a762b
Jean Paul <jean.paul.ruben@gmail.com>
2026-09-17 11:10:22 -0400

    hotfix, honeypot

--- src/features/contact/schemas/contact.schema.ts
+++ src/features/contact/schemas/contact.schema.ts
@@ -5,6 +5,7 @@ export const contactSchema = z.object({
   email: z.email('Invalid email address'),
   subject: z.string().min(2, 'Subject must be at least 2 characters'),
   message: z.string().min(10, 'Message must be at least 10 characters'),
+  website: z.string().max(0),
 });
 
 export type ContactFormData = z.infer<typeof contactSchema>;
